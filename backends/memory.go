86ff723cdf5a492590a00aaf1339895e0df5c8ea
Adam Makowski <79207455+adammakowskidev@users.noreply.github.com>
2024-06-05 17:32:06 +0200

    Update memory.go

--- backends/memory.go
+++ backends/memory.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/mailgun/groupcache/v2"
-	"github.com/sillygod/cdp-cache/pkg/helper"
+	"github.com/adammakowskidev/caddy-cache-engine/pkg/helper"
 )
 
 type ctxKey string
